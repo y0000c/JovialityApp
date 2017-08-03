42c305dd358f77a7ac89971948ad041993cd8bb4
yc <1149743857@qq.com>
2017-08-03 14:55:22 +0800

    添加了网络状态工具类

--- app/src/main/java/demo/yc/joviality/fragment/partofimage/ImageListFragment.java
+++ app/src/main/java/demo/yc/joviality/fragment/partofimage/ImageListFragment.java
@@ -2,8 +2,11 @@ package demo.yc.joviality.fragment.partofimage;
 
 
 import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 
+import butterknife.BindView;
 import demo.yc.joviality.fragment.base.BaseAppFragment;
 import demo.yc.jovialityyc.R;
 import demo.yc.lib.utils.LogUtil;
@@ -13,6 +16,13 @@ import demo.yc.lib.utils.LogUtil;
  */
 public class ImageListFragment extends BaseAppFragment
 {
+
+    @BindView(R.id.image_list_recycler_view)
+    RecyclerView mRecyclerView;
+
+    @BindView(R.id.image_list_refresh_layout)
+    SwipeRefreshLayout mRefreshLayout;
+
     @Override
     protected int getContentLayoutId()
     {
@@ -22,37 +32,37 @@ public class ImageListFragment extends BaseAppFragment
     @Override
     protected void initEvents()
     {
-        LogUtil.d("life",TAG+" initEvents");
+        LogUtil.d("life", TAG + " initEvents");
     }
 
     @Override
     protected void onFirstUserVisible()
     {
-        LogUtil.d("life",TAG+"_onFirstUserVisible");
+        LogUtil.d("life", TAG + "_onFirstUserVisible");
     }
 
     @Override
     protected void onUserVisible()
     {
-        LogUtil.d("life",TAG+" onUserVisible");
+        LogUtil.d("life", TAG + " onUserVisible");
     }
 
     @Override
     protected void onFirstUserInvisible()
     {
-        LogUtil.d("life",TAG+" onFirstUserInvisible");
+        LogUtil.d("life", TAG + " onFirstUserInvisible");
     }
 
     @Override
     protected void onUserInVisible()
     {
-        LogUtil.d("life",TAG+"_onUserInVisible");
+        LogUtil.d("life", TAG + "_onUserInVisible");
     }
 
     @Override
     protected View getLoadingTargetView()
     {
-        return null;
+        return mRefreshLayout;
     }
 
 }
